3384738fed67f70edb40ab6a235b90a7f2c591bb
Luke <luke@arkitek.co.uk>
2020-02-26 14:52:06 -0700

    reorganized file strucutre and added icons

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './Button';
-export { default as Container } from './Container';
-export { default as Text } from './Text';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-export * from './components';
+export { default as Button } from './Button';
+export { default as Container } from './Container';
+export { default as Text } from './Text';
+export { default as Icon } from './Icons';
 
 import * as themes from './styles/theme';
 export { themes };
