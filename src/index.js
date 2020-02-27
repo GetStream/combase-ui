a001df09a86b0c25c8c128e77675eef3f6d2a4e3
Luke <luke@arkitek.co.uk>
2020-02-27 16:13:29 -0700

    export iconbubble from index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { default as Container } from './Container';
 export { default as FAB } from './FAB';
 export { default as Text } from './Text';
 export { default as Icon } from './Icons';
+export { default as IconBubble } from './IconBubble';
 export { default as Portal } from './Portal';
 
 import * as themes from './styles/theme';
