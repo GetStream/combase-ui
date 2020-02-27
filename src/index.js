233d4403576165f97a7fa0c522419cd1f91598fe
Luke <luke@arkitek.co.uk>
2020-02-26 17:20:03 -0700

    Added Card, FAB & Portal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 export { default as Button } from './Button';
+export { default as Card } from './Card';
 export { default as Container } from './Container';
+export { default as FAB } from './FAB';
 export { default as Text } from './Text';
 export { default as Icon } from './Icons';
+export { default as Portal } from './Portal';
 
 import * as themes from './styles/theme';
 export { themes };
