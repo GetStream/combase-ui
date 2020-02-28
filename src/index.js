2edf93591593d17871eeea69835c119660a701c2
Luke <luke@arkitek.co.uk>
2020-02-28 11:47:00 -0700

    export input components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+export { default as AutosizeTextArea } from './AutosizeTextArea';
 export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Container } from './Container';
 export { default as FAB } from './FAB';
+export { default as Input } from './Input';
 export { default as Text } from './Text';
 export { default as Icon } from './Icons';
 export { default as IconBubble } from './IconBubble';
