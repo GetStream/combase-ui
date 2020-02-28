b4171e6fb1217e7845e714eb2aef91fa655118b7
Luke <luke@arkitek.co.uk>
2020-02-28 09:33:56 -0700

    export Avatar & StatusBadge

--- src/StatusBadge/StatusBadge.js
+++ src/StatusBadge/StatusBadge.js
@@ -1,21 +1,22 @@
-import React from "react"; // eslint-disable-line no-unused-vars
+import React from 'react'; // eslint-disable-line no-unused-vars
 import PropTypes from 'prop-types';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StatusBadge = styled.div`
-  width: ${({ size }) => size}px;
-  height: ${({ size }) => size}px;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.color.green};
-  border: ${({ size }) => size / 4}px solid ${({ borderColor, theme }) => theme.color[borderColor]};
+    width: ${({ size }) => size}px;
+    height: ${({ size }) => size}px;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.color.green};
+    border: ${({ size }) => size / 4}px solid
+        ${({ borderColor, theme }) => theme.color[borderColor]};
 `;
 
 StatusBadge.propTypes = {
-  size: PropTypes.number,
-}
+    size: PropTypes.number,
+};
 
 StatusBadge.defaultProps = {
-  size: 16,
-}
+    size: 16,
+};
 
 export default StatusBadge;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Container } from './Container';
@@ -6,6 +7,7 @@ export { default as Text } from './Text';
 export { default as Icon } from './Icons';
 export { default as IconBubble } from './IconBubble';
 export { default as Portal } from './Portal';
+export { default as StatusBadge } from './StatusBadge';
 
 import * as themes from './styles/theme';
 export { themes };
