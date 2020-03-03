a0b713ab2f1494b9cd6942f5b3fcdafc81e9b295
Luke <luke@arkitek.co.uk>
2020-03-03 13:59:04 +0100

    added Fill, Chip & Badge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 export { default as AutosizeTextArea } from './AutosizeTextArea';
 export { default as Avatar } from './Avatar';
+export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as Chip } from './Chip';
 export { default as Container } from './Container';
 export { default as FAB } from './FAB';
 export { default as Fill } from './Fill';
