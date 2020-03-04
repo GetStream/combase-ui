1184d1964f1b93b963d77cdc41ef1018617ec565
Luke <luke@arkitek.co.uk>
2020-03-04 09:23:44 +0100

    export ColorInput

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Chip } from './Chip';
 export { default as CircularProgress } from './CircularProgress';
+export { default as ColorInput } from './ColorInput';
 export { default as Container } from './Container';
 export { default as FAB } from './FAB';
 export { default as Fill } from './Fill';
